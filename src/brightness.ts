ae088c35a3d8f407327aed4281a3c44c14be4717
Mr. Dan <mr.dan.blog@pm.me>
2026-07-08 15:28:40 -0400

    Finally fix svg?

--- src/brightness.ts
+++ src/brightness.ts
@@ -1,14 +1,16 @@
 import Render from "./rendering"
 
 function remap_x(x: number): string {
-    return (x * 180).toString();
+    return (x * 180).toFixed(2);
 }
 
 function remap_y(y: number): string {
-    return ((1 - y) * 180).toString();
+    return ((1 - y) * 180).toFixed(2);
 }
 
 const svgdoc = document.getElementById("svgdoc") as HTMLOrSVGElement as SVGSVGElement;
+
+
 
 const circle_black = svgdoc.getElementById("circle1") as SVGCircleElement;
 const circle_night_max = svgdoc.getElementById("circle2") as SVGCircleElement;
@@ -19,21 +21,21 @@ const connector = svgdoc.getElementById("connector") as SVGPathElement;
 
 export function refresh_brightness(params: Render.Params) {
     const black = remap_x(params.black);
-    circle_black.style.setProperty("cx", black + "px");
+    circle_black.setAttribute("cx", black);
 
     const night_x = remap_x(0.5 - params.gap / 2);
     const night_max = remap_y(params.night_max);
-    circle_night_max.style.setProperty("cx", night_x + "px");
-    circle_night_max.style.setProperty("cy", night_max + "px");
+    circle_night_max.setAttribute("cx", night_x + "px");
+    circle_night_max.setAttribute("cy", night_max + "px");
 
     const day_x = remap_x(0.5 + params.gap / 2);
     const day_min = remap_y(params.day_min);
-    circle_day_min.style.setProperty("cx", day_x + "px");
-    circle_day_min.style.setProperty("cy", day_min + "px");
+    circle_day_min.setAttribute("cx", day_x + "px");
+    circle_day_min.setAttribute("cy", day_min + "px");
 
     const white = remap_x(params.white);
-    circle_white.style.setProperty("cx", white + "px");
+    circle_white.setAttribute("cx", white + "px");
 
-    const connector_string = `path("M0,180 H${black} L${night_x},${night_max} L${day_x},${day_min} L${white}, 0H180")`;
-    connector.style.setProperty("d", connector_string);
+    const connector_string = `M0,180 H${black} L${night_x},${night_max} L${day_x},${day_min} L${white},0 H180`;
+    connector.setAttribute("d", connector_string);
 }
