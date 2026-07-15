e21a6499f0b394508ca63a369dc0e1d2e24fbeca
Mr. Dan <mr.dan.blog@pm.me>
2026-07-15 10:49:23 -0400

    Use idiomatic setAttributeNS(null, args)

--- src/brightness.ts
+++ src/brightness.ts
@@ -1,4 +1,4 @@
-import Render from "./rendering"
+import Render from "./rendering";
 
 function remap_x(x: number): string {
     return (x * 180).toFixed(2);
@@ -21,21 +21,21 @@ const connector = svgdoc.getElementById("connector") as SVGPathElement;
 
 export function refresh_brightness(params: Render.Params) {
     const black = remap_x(params.black);
-    circle_black.setAttribute("cx", black);
+    circle_black.setAttributeNS(null, "cx", black);
 
     const night_x = remap_x(0.5 - params.gap / 2);
     const night_max = remap_y(params.night_max);
-    circle_night_max.setAttribute("cx", night_x + "px");
-    circle_night_max.setAttribute("cy", night_max + "px");
+    circle_night_max.setAttributeNS(null, "cx", night_x + "px");
+    circle_night_max.setAttributeNS(null, "cy", night_max + "px");
 
     const day_x = remap_x(0.5 + params.gap / 2);
     const day_min = remap_y(params.day_min);
-    circle_day_min.setAttribute("cx", day_x + "px");
-    circle_day_min.setAttribute("cy", day_min + "px");
+    circle_day_min.setAttributeNS(null, "cx", day_x + "px");
+    circle_day_min.setAttributeNS(null, "cy", day_min + "px");
 
     const white = remap_x(params.white);
-    circle_white.setAttribute("cx", white + "px");
+    circle_white.setAttributeNS(null, "cx", white + "px");
 
     const connector_string = `M0,180 H${black} L${night_x},${night_max} L${day_x},${day_min} L${white},0 H180`;
-    connector.setAttribute("d", connector_string);
+    connector.setAttributeNS(null, "d", connector_string);
 }
